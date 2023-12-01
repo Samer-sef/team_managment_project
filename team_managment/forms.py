631fb427f4e4d23c0f3dab3881e3b3c2ced06644
Samer Sefrani <ssefrani@sfu.ca>
2023-12-01 12:26:56 -0800

    add forms format validation

--- team_managment/forms.py
+++ team_managment/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from .models import Member
 
 class MemberForm(forms.ModelForm):
-
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    mobile = forms.IntegerField()
+    email = forms.EmailField()
     class Meta:
         model = Member
         # Keep this oder in the UI
