59107dcbe4d038ab04833625bc0532ab78d6fd05
Samer Sefrani <ssefrani@sfu.ca>
2023-11-30 23:11:07 -0800

    Add adding a team member functionality

--- team_managment/models.py
+++ team_managment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+ROLE_CHOICES = (
+        (False, 'Regular - Can\'t delete members'),
+        (True, 'Admin - Can delete members'),
+    )
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=15)
+    role = models.BooleanField(default=False, choices=ROLE_CHOICES)
--- team_managment/views.py
+++ team_managment/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MemberForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'list_page.html', {})
+
+def details(request):
+    if request.method == "GET":
+        form = MemberForm()
+        return render(request, 'details_page.html', {'form': form})
+    else:
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
 
 
