4771edb2cdb559507cc92e60e24c1363177d4fce
Samer Sefrani <ssefrani@sfu.ca>
2023-12-06 16:20:11 -0800

    change from function to generic view

--- team_managment/views.py
+++ team_managment/views.py
@@ -1,30 +1,22 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
 from .forms import MemberForm
 from .models import Member
 
+home_dir = '/'
 
-def home(request):
-    context = {'members_list': Member.objects.all()}
-    return render(request, 'list_page.html', context)
-
-def details(request, id=0):
-    if request.method == "GET":
-        if id == 0:
-            form = MemberForm()
-        else:
-            form = MemberForm(instance=Member.objects.get(pk=id))
-        return render(request, 'details_page.html', {'form': form})
-    else:
-        if id == 0:
-            form = MemberForm(request.POST)
-        else:
-            form = MemberForm(request.POST, instance=Member.objects.get(pk=id))
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-    
-def delete(request, id=0):
-    Member.objects.get(pk=id).delete()
-    return redirect('/')
-
-
+class Home(ListView):
+    model = Member
+    context_object_name = 'members_list'
+class Create(CreateView):
+    model = Member
+    form_class = MemberForm
+    success_url = home_dir
+class Details(UpdateView):
+    model = Member
+    form_class = MemberForm
+    success_url = home_dir
+class Delete(DeleteView):
+    model = Member
+    context_object_name = 'member'
+    success_url = home_dir
